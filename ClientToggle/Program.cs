6843aeddfe6134b7942b32e7dd93aa8ae382c2c6
Thiago Holder <thiagoholder@gmail.com>
2017-08-17 23:04:36 -0300

    Securoty Password Flow

--- ClientToggle/Program.cs
+++ ClientToggle/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
 namespace ClientToggle
@@ -16,37 +18,27 @@ namespace ClientToggle
         {
             var client = new HttpClient();
 
-            try
-            {
-                var token = await GetTokenAsync(client);
-                Console.WriteLine("Access token: {0}", token);
-                Console.WriteLine();
+            const string email = "administrator@toggleservice.com", password = "sW5brAwaCu=a";
 
-                var resource = await GetResourceAsync(client, token);
-                Console.WriteLine("API response: {0}", resource);
-                Console.ReadLine();
-            }
-            catch (System.Net.Http.HttpRequestException ex)
-            {
-                var builder = new StringBuilder();
-                builder.AppendLine("+++++++++++++++++++++");
-                builder.AppendLine(ex.Message);
-                builder.AppendLine(ex.InnerException.Message);
-                builder.AppendLine("Make sure you started the authorization server.");
-                builder.AppendLine("+++++++++++++++++++++");
-                Console.WriteLine(builder.ToString());
-            }
+            var token = await GetTokenAsync(client, email, password);
+            Console.WriteLine("Access token: {0}", token);
+            Console.WriteLine();
+
+            var resource = await GetResourceAsync(client, token);
+            Console.WriteLine("API response: {0}", resource);
+
+            Console.ReadLine();
         }
 
-        public static async Task<string> GetTokenAsync(HttpClient client)
+        public static async Task<string> GetTokenAsync(HttpClient client, string email, string password)
         {
             var request = new HttpRequestMessage(HttpMethod.Post, "http://localhost:57425/connect/token")
             {
                 Content = new FormUrlEncodedContent(new Dictionary<string, string>
                 {
-                    ["grant_type"] = "client_credentials",
-                    ["client_id"] = "ServiceB",
-                    ["client_secret"] = "388D45FA-B36B-4988-BA59-B187D329C207"
+                    ["grant_type"] = "password",
+                    ["username"] = email,
+                    ["password"] = password
                 })
             };
 
@@ -64,7 +56,7 @@ namespace ClientToggle
 
         public static async Task<string> GetResourceAsync(HttpClient client, string token)
         {
-            var request = new HttpRequestMessage(HttpMethod.Get, "http://localhost:57425/api/features");
+            var request = new HttpRequestMessage(HttpMethod.Get, "http://localhost:57425/api/administrator/toggles");
             request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
 
             var response = await client.SendAsync(request, HttpCompletionOption.ResponseContentRead);
